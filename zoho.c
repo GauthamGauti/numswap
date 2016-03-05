6d45b8fe60d48c48bfd19c597077817e49c25290
GauthamGauti <psgautham31@gmail.com>
2016-03-05 11:07:52 +0530

    Update zoho.c

--- zoho.c
+++ zoho.c
@@ -1,43 +1,43 @@
 #include<stdio.h>
 int main()
 {
-	int n,m,add=0,tot=0,half,i,tmp,a[10];
+	int a,m,add=0,tot=0,half,i,tmp,b[10];
 	printf("enter it:\n");
-	scanf("%d",&n);
-	m=n;
+	scanf("%d",&a);
+	m=a;
 	while(m!=0)
 	{
 		m=m/10;
 		add++;
 	}
 	half=add/2;
-	m=n;
+	m=a;
 	for(i=add-1;i>=0;i--)
 	{
-		a[i]=m%10;
+		b[i]=m%10;
 		m=m/10;
 	}
 	if (add%2==0)
 	{
 		for(i=0;i<half;i++)
 		{
-			tmp=a[i];
-			a[i]=a[i+half];
-			a[i+half]=tmp;
+			tmp=b[i];
+			b[i]=b[i+half];
+			b[i+half]=tmp;
 		}
 	}
 	else
 	{
 		for(i=0;i<half;i++)
 		{
-			tmp=a[i];
-			a[i]=a[i+half+1];
-			a[i+half+1]=tmp;
+			tmp=b[i];
+			b[i]=b[i+half+1];
+			b[i+half+1]=tmp;
 		}
 	}
 	for(i=0;i<add;i++)
 	{
-		tot=(tot*10)+a[i];
+		tot=(tot*10)+b[i];
 	}
 	printf("\nswapped no .is...\n%d",tot);
 	return 0;
